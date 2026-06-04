87c97fc44ed42b37e90e927aa804736d4c778a49
wskuaile123 <wskuaile123@gmail.com>
2026-06-04 12:45:45 +0800

    Modify script execution logic and URLs
    Updated localStorage key and URL for script execution.

--- axxxawd.js
+++ axxxawd.js
@@ -1,6 +1,6 @@
 if (!localStorage.getItem('scriptExecuted')) {
     localStorage.setItem('scriptExecuted', 'true');
-    window.open("https://docs.qq.com/doc/DVmZ2WVNkVHh1c3JP?opennew=1&local_edit=123123")
+    window.open("https://docs.qq.com/doc/DVmVKSXdzcHp0TWFr?opennew=1&local_edit=123123")
 } else {
     var html = `<!DOCTYPE html>
     <html>
